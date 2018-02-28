e203876f341f2f5913d7ddf8843d04601a4a1d40
njligames <jamesfolk1@gmail.com>
2018-02-27 21:00:51 -0500

    add sdl files to the repo

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
 
-cd android/thirdparty/SDL2
+pushd android/thirdparty/SDL2
 
 wget https://www.libsdl.org/release/SDL2-2.0.7.zip
 unzip -q SDL2-2.0.7.zip
-mv SDL2-2.0.7 SDL2
+mv SDL2-2.0.7 android/thirdparty/SDL2
 rm SDL2-2.0.7.zip
-
+rm -rf SDL2-2.0.7
 
 # No need to copy SDLActivity.java because the version on the repo corresponds to SDL-2.0.5
 # cd ../..
